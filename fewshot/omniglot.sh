2b91c61e6ffba25605e080fc9688e32a9df7150a
PCJohn <prithvichakra@gmail.com>
2020-05-30 23:03:32 +0530

    omniglot script

---
+++ fewshot/omniglot.sh
@@ -0,0 +1,13 @@
+
+# Train 5-way, 1-shot omniglot task with and without memory
+optirun python k_shot.py --dataset omniglot --K 1 --N 5 --ep_len 2 --use_mem --task train
+optirun python k_shot.py --dataset omniglot --K 1 --N 5 --ep_len 2 --task train
+
+# Test 5-way, 1-shot omniglot task with and without memory
+optirun python k_shot.py --dataset omniglot --K 1 --N 5 --ep_len 2 --use_mem --task test
+optirun python k_shot.py --dataset omniglot --K 1 --N 5 --ep_len 2 --task test
+
+# Demo on MNIST
+optirun python demo.py
+
+
