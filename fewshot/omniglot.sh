2c7de445dddf5153a0f5e068102fe70ac3dfe9ac
PCJohn <prithvichakra@gmail.com>
2020-05-30 23:50:43 +0530

    run omniglot setup before train, test

--- fewshot/omniglot.sh
+++ fewshot/omniglot.sh
@@ -1,3 +1,5 @@
+# Download and setup omniglot
+sh setup.sh
 
 # Train 5-way, 1-shot omniglot task with and without memory
 optirun python k_shot.py --dataset omniglot --K 1 --N 5 --ep_len 2 --use_mem --task train
@@ -6,8 +8,5 @@ optirun python k_shot.py --dataset omniglot --K 1 --N 5 --ep_len 2 --task train
 # Test 5-way, 1-shot omniglot task with and without memory
 optirun python k_shot.py --dataset omniglot --K 1 --N 5 --ep_len 2 --use_mem --task test
 optirun python k_shot.py --dataset omniglot --K 1 --N 5 --ep_len 2 --task test
-
-# Demo on MNIST
-optirun python demo.py
 
 
